7de47f2286c619331f4c49341a8237272ee36130
korov2000 <63860438+korov2000@users.noreply.github.com>
2022-11-30 19:09:59 +0100

    added menu scene

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -18,7 +18,24 @@ public class MainManager : MonoBehaviour
     
     private bool m_GameOver = false;
 
-    
+    public static MainManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // napiši metodu koja će u varijablu strpati ima igrača
+    // pozivaj je iz Awake() metode
+    // metoda mora mora zapisati ime u varijablu kada se klikne na Start Game button
+
     // Start is called before the first frame update
     void Start()
     {
