a7b961914e8e14652002304ca1205c6d96b37446
korov2000 <63860438+korov2000@users.noreply.github.com>
2022-12-01 22:29:30 +0100

    added scene persistence

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -18,24 +18,6 @@ public class MainManager : MonoBehaviour
     
     private bool m_GameOver = false;
 
-    public static MainManager Instance;
-
-    private void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
-
-    // napiši metodu koja će u varijablu strpati ima igrača
-    // pozivaj je iz Awake() metode
-    // metoda mora mora zapisati ime u varijablu kada se klikne na Start Game button
-
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // napiši metodu koja će u varijablu strpati ima igrača
+    // pozivaj je iz Awake() metode
+    // metoda mora mora zapisati ime u varijablu kada se klikne na Start Game button
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
